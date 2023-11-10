88a25588a2ed86d37c2130b585f341ed0a6078ac
Nick Blomberg <nick.blomberg@gmail.com>
2023-11-10 21:23:28 +0000

    Extract Chapter into separate component

---
+++ app/components/Chapter.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Nothing_You_Could_Do } from 'next/font/google'
+
+const titleFont = Nothing_You_Could_Do({ subsets: ['latin'], weight: '400' })
+
+function Chapter({ chapterTitle, chapterQuote, chapterContent }) {
+  return (
+    <main className='flex flex-col items-center gap-6 px-3 pt-8 md:pt-32'>
+      <h1 className={`text-4xl font-bold ${titleFont.className}`}>
+        {chapterTitle}
+      </h1>
+      <div className='text-center text-sm italic leading-6 tracking-wider text-battleship-gray-500'>
+        {chapterQuote.map((line, key) => (
+          <p key={key}>{line}</p>
+        ))}
+      </div>
+
+      <div className='max-w-prose'>
+        {chapterContent.map((paragraph, key) => (
+          <p key={key} className='pb-3'>
+            {paragraph}
+          </p>
+        ))}
+      </div>
+    </main>
+  )
+}
+
+export default Chapter
--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
-import { Nothing_You_Could_Do } from 'next/font/google'
-
-const titleFont = Nothing_You_Could_Do({ subsets: ['latin'], weight: '400' })
+import Chapter from './components/Chapter'
 
 import data from '../data.json'
 
@@ -9,28 +7,6 @@ const currentDayNumber = Math.ceil(
     (24 * 60 * 60 * 1000),
 )
 
-const { chapterNumber, chapterTitle, chapterQuote, chapterContent } =
-  data[currentDayNumber]
-
 export default function Home() {
-  return (
-    <main className='flex flex-col items-center gap-6 px-3 pt-8 md:pt-32'>
-      <h1 className={`text-4xl font-bold ${titleFont.className}`}>
-        {chapterTitle}
-      </h1>
-      <div className='text-center text-sm italic leading-6 tracking-wider text-battleship-gray-500'>
-        {chapterQuote.map((line, key) => (
-          <p key={key}>{line}</p>
-        ))}
-      </div>
-
-      <div className='max-w-prose'>
-        {chapterContent.map((paragraph, key) => (
-          <p key={key} className='pb-3'>
-            {paragraph}
-          </p>
-        ))}
-      </div>
-    </main>
-  )
+  return <Chapter {...data[currentDayNumber]} />
 }
