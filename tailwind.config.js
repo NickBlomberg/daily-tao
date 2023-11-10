afd6846d647b217747aaca4fd6963dab81252ad2
Nick Blomberg <nick.blomberg@gmail.com>
2023-11-10 14:08:25 +0000

    Add page to display chapter for the current day

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'battleship-gray': {
+          50: '#F8F9F8',
+          100: '#F2F4F0',
+          200: '#DEE3DB',
+          300: '#CBD1C5',
+          400: '#A3AF99',
+          500: '#7C8D6D',
+          600: '#707F62',
+          700: '#4A5541',
+          800: '#383F31',
+          900: '#252A21',
+        },
+      },
     },
   },
   plugins: [],
