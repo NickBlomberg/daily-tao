a1f0f0c3b9881e7fac39904400b8342c2d1b1b45
Nick Blomberg <nick.blomberg@gmail.com>
2023-11-10 04:04:36 +0000

    Create script to seed data

---
+++ seed.js
@@ -0,0 +1,87 @@
+const fs = require('fs')
+const cheerio = require('cheerio')
+const EPub = require('epub')
+
+const EPUB_FILE_PATH =
+  './Ming-Dao Deng - 365 Tao_ Daily Meditations-Harper (1992).epub'
+const book = new EPub(EPUB_FILE_PATH)
+
+const data = {}
+
+const processChapter = (item) => {
+  return new Promise((resolve, reject) => {
+    book.getChapter(item.id, (error, text) => {
+      if (error) {
+        reject(`Error getting content for item ${item.id}: ${error}`)
+      }
+
+      const $ = cheerio.load(text)
+
+      const chapterNumber = $('h1.chnumber').last().text()
+      const chapterTitle = $('h1.chtitle').last().text()
+
+      const quoteDiv = $('div.blockquote')
+      const chapterQuote = quoteDiv
+        .find('p.bq')
+        .map((index, element) => $(element).text())
+        .get()
+        .join('<br />')
+
+      const chapterContent = quoteDiv
+        .nextAll('p')
+        .map((index, element) => $(element).text())
+        .get()
+        .join('<br />')
+
+      resolve({
+        chapterNumber,
+        chapterTitle,
+        chapterQuote,
+        chapterContent,
+      })
+    })
+  })
+}
+
+const processAllChapters = async () => {
+  const FIRST_CHAPTER = 7
+  const LAST_CHAPTER = 372
+
+  const promises = book.flow
+    .slice(FIRST_CHAPTER, LAST_CHAPTER)
+    .map(processChapter)
+
+  try {
+    const chaptersData = await Promise.all(promises)
+
+    chaptersData.forEach((chapterData, index) => {
+      data[chapterData.chapterNumber] = chapterData
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const main = async () => {
+  return new Promise((resolve, reject) => {
+    book.on('end', async () => {
+      try {
+        await processAllChapters()
+        resolve()
+      } catch (error) {
+        reject(error)
+      }
+    })
+
+    book.parse()
+  })
+}
+
+main()
+  .then(() => {
+    const OUTPUT_FILE = 'data.json'
+    fs.writeFileSync(OUTPUT_FILE, JSON.stringify(data, null, 2), 'utf-8')
+  })
+  .catch((error) => {
+    console.error('Error processing EPUB:', error)
+  })
